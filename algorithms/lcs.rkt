b2fb2a1b6737e480ea6ffc0740e89b83e4171bf6
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-10-24 10:32:49 -0300

    refatoring lcs

--- algorithms/lcs.rkt
+++ algorithms/lcs.rkt
@@ -1,38 +1,63 @@
 #lang racket
 (require rackunit)
 (require rackunit/text-ui)
+(require memo)
 (provide lcs)
 
-(define (longest xlist1 xlist2)
-  (if (> (length xlist1) (length xlist2))
-      xlist1 xlist2))
- 
-(define memo (make-hash))
-
-(define (lookup xlist1 xlist2)
-  (hash-ref memo (cons xlist1 xlist2) #f))
-(define (store xlist1 xlist2 r)
-  (hash-set! memo (cons xlist1 xlist2) r)
-  r)
- 
-(define (lcs-list list1 list2)
-  (or (lookup list1 list2)
-    (store list1 list2
-      (match* (list1 list2)
-        [((cons x xlist1) (cons y xlist2))
-        (if (equal? x y)
-          (cons x (lcs-list xlist1 xlist2))
-          (longest (lcs-list list1 xlist2) (lcs-list xlist1 list2)))]
-        [(_ _) '()]))))
+(define/memoize (lcs-list list1 list2 indice1 indice2)
+  (cond 
+    [(or 
+      (equal? indice1 0)
+      (equal? indice2 0))
+      0
+    ]
+    [(equal? 
+      (list-ref list1 (- indice1 1))
+      (list-ref list2 (- indice2 1)))
+      (+
+        1
+        (lcs-list
+          list1
+          list2
+          (- indice1 1)
+          (- indice2 1)
+        )
+      )  
+    ]
+    [else
+      (max
+        (lcs-list
+          list1
+          list2
+          indice1
+          (- indice2 1)
+        )
+        (lcs-list
+          list1
+          list2
+          (- indice1 1)
+          indice2
+        )
+      )
+    ]
+  )
+)
 
 (define (lcs text1 text2)
   (exact->inexact 
     (/ 
-      (length (lcs-list (string->list text1) (string->list text2)))
+      (lcs-list 
+        (string->list text1)
+        (string->list text2)
+        (string-length text1)
+        (string-length text2)
+      )
       (max 
-        (length (string->list text1))
-        (length (string->list text1))
+        (string-length text1)
+        (string-length text2)
       )
     )
   )
 )
+
+(lcs "AGGTAB" "GXTXAYB")
