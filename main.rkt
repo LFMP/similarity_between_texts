2c3784d133bd7167da06be05f7aeb9f70c4821fd
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-10-25 20:06:32 -0300

    reading input

--- main.rkt
+++ main.rkt
@@ -1,3 +1,11 @@
 #lang racket
 (require "algorithms/dice.rkt")
 (require "algorithms/lcs.rkt")
+
+(define text1 (read-line (current-input-port) 'any))
+(define text2 (read-line (current-input-port) 'any))
+
+(display "Similaridade por LCS: ")
+(println (lcs text1 text2))
+(display "Similaridade por Dice's Coefficient: ")
+(println (dice text1 text2))
