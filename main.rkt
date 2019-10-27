cbbfcb33d1a49b0f8ad16fca78516e49b8b56237
Luiz Fellipe Machi Pereira <luizfellipe42@hotmail.com>
2019-10-26 23:36:37 -0300

    add texts, docs, and modified main for read files

--- main.rkt
+++ main.rkt
@@ -2,10 +2,34 @@
 (require "algorithms/dice.rkt")
 (require "algorithms/lcs.rkt")
 
-(define text1 (read-line (current-input-port) 'any))
-(define text2 (read-line (current-input-port) 'any))
+(define text1 (file->string "texts/text1.txt"))
+(define text2 (file->string "texts/text2.txt"))
 
 (display "Similaridade por LCS: ")
 (println (lcs text1 text2))
 (display "Similaridade por Dice's Coefficient: ")
 (println (dice text1 text2))
+
+(define text3 (file->string "texts/text3.txt"))
+(define text4 (file->string "texts/text4.txt"))
+
+(display "Similaridade por LCS: ")
+(println (lcs text3 text4))
+(display "Similaridade por Dice's Coefficient: ")
+(println (dice text3 text4))
+
+(define text5 (file->string "texts/text5.txt"))
+(define text6 (file->string "texts/text6.txt"))
+
+(display "Similaridade por LCS: ")
+(println (lcs text5 text6))
+(display "Similaridade por Dice's Coefficient: ")
+(println (dice text5 text6))
+
+(define text7 (file->string "texts/text7.txt"))
+(define text8 (file->string "texts/text8.txt"))
+
+(display "Similaridade por LCS: ")
+(println (lcs text7 text8))
+(display "Similaridade por Dice's Coefficient: ")
+(println (dice text7 text8))
